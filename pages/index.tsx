8512b44fb4dd973c76c351c885e5881e8a2ca50f
Jakub Juszczyk <jakub.juszczyk@flipfit.com>
2022-07-20 10:36:46 +0200

    add title

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,13 @@ import Link from "next/link";
 const Home: NextPage = () => {
   return (
     <div>
-      <h1>Homepage</h1>
+      <h1>Clicking on a link with query causes an additional request to Next before redirection</h1>
       <ul>
         <li>
           <Link href="/dummy/1">/dummy/1</Link>
         </li>
         <li>
-          <Link href={"/dummy/2?a=a"}>/dummy/2?a=a</Link>
+          <Link href={"/dummy/2?a=a"}>/dummy/2?a=a [broken cache hit]</Link>
         </li>
       </ul>
     </div>
